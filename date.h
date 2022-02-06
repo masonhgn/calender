6a97a16145d59ed0ab0e01ecd3aadb460be60944
Mason <73012906+masonhgn@users.noreply.github.com>
2022-02-06 00:44:46 -0500

    second actual actual commit

--- date.h
+++ date.h
@@ -3,7 +3,7 @@ class Date {
     int GetDay();
     int GetYear();
     bool Set(int m, int d, int y);
-    bool SetFormat(char f);
+
     int Compare(const Date& d);
     friend bool validDate(int month, int day);
 public:
@@ -11,7 +11,7 @@ public:
     Date(int m, int d, int y);
     void Increment(int numDays = 1);
     void Show();
-
+    bool SetFormat(char f);
 
 private:
     char format; //format for date, either default, 2 digit or long
--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 int main() {
-    Date d1(2,4,2000);
-    d1.Increment(503);
+    Date d1(2,4,2001);
+    d1.SetFormat('t');
+    d1.Increment(801);
+    d1.Show();
+    d1.Increment(32);
+    d1.Show();
+    d1.Increment(6739);
+    d1.Show();
+    d1.Increment(67349);
+    d1.Show();
+
+
     return 0;
 }
 
